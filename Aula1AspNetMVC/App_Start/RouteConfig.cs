925cc286e6d5802c81f74d915ac90e46ebb7e25f
Hercules-Macedo <herculesmb17@hotmail.com>
2017-09-13 13:46:52 -0300

    init

---
+++ Aula1AspNetMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Aula1AspNetMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            //criando outras rotas
+            //não pode haver parametros opcionais se o ultimo parametro não for opcional, 
+            //para que n se troque a ordem dos parametros e se entenda um parametro opcional no lugar do proximo parametro
+            routes.MapRoute(
+                name: "Secundaria",
+                url: "{controller}/{action}/{id}/{nome}",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional,
+                    nome = UrlParameter.Optional
+                });
+
+            //rota padrão sempre tem que ser a ultima 
+            routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        );
+        }
+    }
+}
---
+++ Aula1AspNetMVC/Controllers/ClienteController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Aula1AspNetMVC.Models;
+
+namespace Aula1AspNetMVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = new Cliente()
+            {
+                Nome = "ASP",
+                Sobrenome = "NET",
+                DataCadastro = DateTime.Now,
+                Id = 1
+            };
+
+            return View(cliente);
+        }
+
+        //indicar uma view mesmo que a action fuja do padrão da rota
+        //construtor da view aceita mais parametros
+        public ActionResult Teste()
+        {
+            var cliente = new Cliente()
+            {
+                Nome = "ASP",
+                Sobrenome = "NET",
+                DataCadastro = DateTime.Now,
+                Id = 1
+            };
+
+            //ViewBags podem receber qlqr nome, são dinamicas}
+            // ViewBag.Cliente = cliente;
+
+            //ViewData["Cliente"] = cliente;
+
+            ViewData["Cliente"] = "Texto";
+
+            return View("Index",cliente);
+        }
+
+        //action de lista
+        public ActionResult Lista()
+        {
+            var listaClientes = new List<Cliente>()
+            {
+                new Cliente() { Nome = "João", Sobrenome = "Pedro", DataCadastro = DateTime.Now,Id = 1},
+                new Cliente() { Nome = "Fulano", Sobrenome = "Beltrano", DataCadastro = DateTime.Now, Id = 2}
+            };
+
+            return View(listaClientes);
+        }
+
+        //action de pesquisa
+        //parametro tem que ser chamado de id, pois está assim na rota
+        public ActionResult Pesquisa(int? id) //Também poderia ser assim(int id = 0) //pode passar mais de um parametro (int id, string nome)
+        {
+            var listaClientes = new List<Cliente>()
+            {
+                new Cliente() { Nome = "João", Sobrenome = "Pedro", DataCadastro = DateTime.Now,Id = 1},
+                new Cliente() { Nome = "Fulano", Sobrenome = "Beltrano", DataCadastro = DateTime.Now, Id = 2},
+                new Cliente() { Nome = "Eduardo", Sobrenome = "Pires", DataCadastro = DateTime.Now,Id = 3},
+                new Cliente() { Nome = "Aluno", Sobrenome = "Favorito", DataCadastro = DateTime.Now, Id = 4}
+            };
+
+            var cliente = listaClientes.Where(c => c.Id == id).ToList();
+
+            if (!cliente.Any())
+            {
+                TempData["Erro"] = "Nenhum cliente selecionado";
+                return RedirectToAction("ErroPesquisa");
+            }
+
+            return View("Lista",cliente);
+        }
+
+        public ActionResult ErroPesquisa()
+        {
+            return View("Error");
+        }
+    }
+}
